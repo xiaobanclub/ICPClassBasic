c5ec7cf3697b84759a1eb0875945263e1f6af695
jasonruan <jasonruan@tencent.com>
2022-03-12 12:18:38 +0800

    add microblog

---
+++ microblog/src/declarations/microblog/microblog.did.js
@@ -0,0 +1,12 @@
+export const idlFactory = ({ IDL }) => {
+  const Time = IDL.Int;
+  const Message = IDL.Record({ 'msg' : IDL.Text, 'time' : Time });
+  return IDL.Service({
+    'follow' : IDL.Func([IDL.Principal], [], []),
+    'follows' : IDL.Func([], [IDL.Vec(IDL.Principal)], ['query']),
+    'post' : IDL.Func([IDL.Text], [], []),
+    'posts' : IDL.Func([Time], [IDL.Vec(Message)], ['query']),
+    'timeline' : IDL.Func([Time], [IDL.Vec(Message)], []),
+  });
+};
+export const init = ({ IDL }) => { return []; };
